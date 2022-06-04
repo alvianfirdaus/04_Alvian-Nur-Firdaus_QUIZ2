3a55be321cf2b2ba53ddf3f087ff06cf23eecd1d
alvianfirdaus <alvianfirdaus896@gmail.com>
2022-06-04 09:32:03 +0700

    Kuis2

---
+++ kuis2/doublelinkedlistMain_4.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuis2;
+
+/**
+ *
+ * @author Alvian
+ */
+public class doublelinkedlistMain_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void header(){
+        System.out.println("++++++++++++++++++++++++++++++++++++++");
+        System.out.println("Quiz 2 PRAKTIKUM ASD TI-1G dan TI-1F  ");
+        System.out.println("++++++++++++++++++++++++++++++++++++++");
+        System.out.println("Dibuat oleh\t\t: ALVIAN NUR FIRDAUS");
+        System.out.println("NIM\t\t\t: 2141720022");
+        System.out.println("Absen\t\t: 04");
+        System.out.println("++++++++++++++++++++++++++++++++++++++");
+        System.out.println("Sistem antrian resto delish");
+    }
+    
+    public static void menu(){
+        System.out.println("++++++++++++++++++++++++++++++++++++++");
+        System.out.println("Menu");
+        System.out.println(" 1. Tambah Data\n 2. Cetak Antrian\n 3. hapus Antrian\n 4. Laporan Pengurutan Pesanan BY Nama\n 5. Hitung total pendapatan\n 6. Keluar\n");
+        System.out.println("-----------------------------------");
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
